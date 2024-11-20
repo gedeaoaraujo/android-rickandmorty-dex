0bf39e05e6cf08d0086583efaacb29f07a2236f7
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-20 13:05:02 -0300

    Criando projeto

---
+++ app/src/main/java/com/acmelabs/rickandmortydex/presentation/HomeScreen.kt
@@ -0,0 +1,28 @@
+package com.acmelabs.rickandmortydex.presentation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.acmelabs.rickandmortydex.ui.theme.RickAndMortyDexTheme
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(text = "Hello World!")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RickAndMortyDexTheme {
+        HomeScreen()
+    }
+}
