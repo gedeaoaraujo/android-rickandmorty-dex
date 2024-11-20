0c37446fe3a004f194780831507239b0adba813d
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-20 17:56:35 -0300

    Testando serviço de obter todos os personagens.

--- app/src/test/java/com/acmelabs/rickandmortydex/CharacterServiceTest.kt
+++ app/src/test/java/com/acmelabs/rickandmortydex/CharacterServiceTest.kt
@@ -4,14 +4,29 @@ import com.acmelabs.rickandmortydex.data.network.character_service.CharacterServ
 import com.acmelabs.rickandmortydex.data.network.rickAndMortyClient
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 
 class CharacterServiceTest {
+
+    private lateinit var client: CharacterService
+
+    @Before
+    fun setUp(){
+        client = rickAndMortyClient(CharacterService::class.java)
+    }
+
     @Test
     fun `Deve obter 1 personagem com id igual a 1`() = runBlocking {
-        val client = rickAndMortyClient(CharacterService::class.java)
         val response = client.getCharacterById(1).body()
         assertEquals(1, response?.id)
+    }
+
+    @Test
+    fun `Deve retornar mais de 1 personagem`() = runBlocking {
+        val response = client.getAllCharacters().body()
+        val charactersCount = response?.info?.count ?: 0
+        assert(charactersCount > 1)
     }
 }
