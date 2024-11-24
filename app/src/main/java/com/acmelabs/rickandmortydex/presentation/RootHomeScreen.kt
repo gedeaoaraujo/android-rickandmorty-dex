16909c4af439b0da0961b3e846bbb54572534f53
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-24 18:54:37 -0300

    Melhorias na TopAppBar

--- app/src/main/java/com/acmelabs/rickandmortydex/presentation/RootHomeScreen.kt
+++ app/src/main/java/com/acmelabs/rickandmortydex/presentation/RootHomeScreen.kt
@@ -2,20 +2,22 @@ package com.acmelabs.rickandmortydex.presentation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import com.acmelabs.rickandmortydex.presentation.theme.RickAndMortyDexTheme
 import org.koin.androidx.compose.koinViewModel
 
@@ -26,16 +28,16 @@ fun RootHomeScreen() {
     val characters by viewModel.characters.collectAsState()
     RickAndMortyDexTheme {
         Scaffold(
+            modifier = Modifier.padding(WindowInsets.statusBars.asPaddingValues()),
             topBar = {
-                TopAppBar(title = {
-                    Text(
-                        text = "Rick & Morty",
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth()
+                CenterAlignedTopAppBar(
+                    title = { Text(text = "Rick & Morty") },
+                    colors = TopAppBarDefaults.topAppBarColors().copy(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = MaterialTheme.colorScheme.onPrimary
                     )
-                })
-            },
-            modifier = Modifier.fillMaxSize(),
+                )
+            }
         ) { innerPadding ->
             Box(
                 contentAlignment = Alignment.Center,
