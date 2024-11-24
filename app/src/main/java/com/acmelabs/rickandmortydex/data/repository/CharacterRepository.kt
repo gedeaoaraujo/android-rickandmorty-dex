e44d28d492a060820ee6f01900ce0de13688c680
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-24 13:50:49 -0300

    Utilizando mapper de personagem.

--- app/src/main/java/com/acmelabs/rickandmortydex/data/repository/CharacterRepository.kt
+++ app/src/main/java/com/acmelabs/rickandmortydex/data/repository/CharacterRepository.kt
@@ -1,15 +1,17 @@
 package com.acmelabs.rickandmortydex.data.repository
 
-import com.acmelabs.rickandmortydex.data.network.character_service.CharacterResponse
 import com.acmelabs.rickandmortydex.data.network.character_service.CharacterService
+import com.acmelabs.rickandmortydex.data.toModel
+import com.acmelabs.rickandmortydex.domain.model.CharacterModel
 
 class CharacterRepository(
     private val characterService: CharacterService
 ) {
 
-    suspend fun getAllCharacters(): List<CharacterResponse> {
+    suspend fun getAllCharacters(): List<CharacterModel> {
         val response = characterService.getAllCharacters()
-        return response.body()?.results.orEmpty()
+        val models = response.body()?.results?.map { it.toModel() }
+        return models.orEmpty()
     }
 
 }
