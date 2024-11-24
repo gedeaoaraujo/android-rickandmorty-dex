c643bb4ee7a20865307c941e5797b09088e86581
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-24 17:14:48 -0300

    Definindo referência para a cor de fundo.

--- app/src/main/java/com/acmelabs/rickandmortydex/presentation/RootHomeScreen.kt
+++ app/src/main/java/com/acmelabs/rickandmortydex/presentation/RootHomeScreen.kt
@@ -1,10 +1,12 @@
 package com.acmelabs.rickandmortydex.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -40,6 +42,7 @@ fun RootHomeScreen() {
                 modifier = Modifier
                     .padding(innerPadding)
                     .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
             ) {
                 HomeScreen(characters)
             }
