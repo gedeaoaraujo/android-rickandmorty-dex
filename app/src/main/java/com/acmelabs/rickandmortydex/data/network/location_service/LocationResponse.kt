9ead9356b6114cd631c516df4d414e8f59f75cbf
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-29 01:20:11 -0300

    Adicionando novos campos

--- app/src/main/java/com/acmelabs/rickandmortydex/data/network/location_service/LocationResponse.kt
+++ app/src/main/java/com/acmelabs/rickandmortydex/data/network/location_service/LocationResponse.kt
@@ -1,6 +1,11 @@
 package com.acmelabs.rickandmortydex.data.network.location_service
 
 data class LocationResponse(
+    val id: Int?,
     val name: String,
-    val url: String
+    val type: String?,
+    val dimension: String?,
+    val residents: List<String>?,
+    val url: String,
+    val created: String?
 )
