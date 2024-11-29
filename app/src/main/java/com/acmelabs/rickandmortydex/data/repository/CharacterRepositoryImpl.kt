62467c19514f70a524f6902a9fbdb2ddedec068d
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-29 00:38:45 -0300

    Inserindo personagens ao obter-los remotamente.

--- app/src/main/java/com/acmelabs/rickandmortydex/data/repository/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/acmelabs/rickandmortydex/data/repository/CharacterRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.acmelabs.rickandmortydex.data.repository
 
+import com.acmelabs.rickandmortydex.data.database.CharacterDao
 import com.acmelabs.rickandmortydex.data.network.character_service.CharacterService
+import com.acmelabs.rickandmortydex.data.toEntity
 import com.acmelabs.rickandmortydex.data.toModel
 import com.acmelabs.rickandmortydex.domain.model.CharacterModel
 import com.acmelabs.rickandmortydex.domain.repository.CharacterRepository
@@ -9,13 +11,17 @@ import com.acmelabs.rickandmortydex.domain.repository.safeCall
 import com.acmelabs.rickandmortydex.domain.repository.wrapHttpStatus
 
 class CharacterRepositoryImpl(
+    private val characterDao: CharacterDao,
     private val characterService: CharacterService
 ): CharacterRepository {
 
     override suspend fun getAllCharacters(): Status<List<CharacterModel>> {
         val result = safeCall { characterService.getAllCharacters() }
-        val list = result.body?.results?.map { it.toModel() }.orEmpty()
-        return wrapHttpStatus(list, result.code)
+        val list = result.body?.results.orEmpty()
+        val entities = list.map { it.toEntity() }
+        characterDao.upsert(entities)
+        val models = list.map { it.toModel() }
+        return wrapHttpStatus(models, result.code)
     }
 
 }
