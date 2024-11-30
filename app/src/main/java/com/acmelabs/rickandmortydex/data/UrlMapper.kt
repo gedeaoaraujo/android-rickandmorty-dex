15c85d32ac811ef1520ca67bcb97bff2176a88fe
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-30 17:04:08 -0300

    Adicionando conversores de Url pra Id.

---
+++ app/src/main/java/com/acmelabs/rickandmortydex/data/UrlMapper.kt
@@ -0,0 +1,15 @@
+package com.acmelabs.rickandmortydex.data
+
+import android.net.Uri
+
+fun urlToId(url: String?): Int? {
+    return if (url.isNullOrBlank()) null else
+        Uri.parse(url).lastPathSegment?.toInt()
+            ?: throw Throwable("Url inválida")
+}
+
+fun urlsToId(urls: List<String>?): String {
+    return urls
+        ?.mapNotNull { Uri.parse(it).lastPathSegment }
+        ?.toString() ?: throw Throwable("Url inválida")
+}
