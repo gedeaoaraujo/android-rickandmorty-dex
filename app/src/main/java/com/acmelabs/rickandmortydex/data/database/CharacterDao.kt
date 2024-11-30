06db2f8173faf145955c47124f7763f2d8346e2d
gedeoaraujo <gedeao.lemobs@gmail.com>
2024-11-30 17:51:25 -0300

    Utilizando relação de personagens com localizações pra obter os dados.

--- app/src/main/java/com/acmelabs/rickandmortydex/data/database/CharacterDao.kt
+++ app/src/main/java/com/acmelabs/rickandmortydex/data/database/CharacterDao.kt
@@ -1,12 +1,20 @@
 package com.acmelabs.rickandmortydex.data.database
 
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
 import com.acmelabs.rickandmortydex.data.database.entity.CharacterEntity
+import com.acmelabs.rickandmortydex.data.database.relation.CharacterLocationRelation
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CharacterDao {
 
     @Upsert
     suspend fun upsert(characters: List<CharacterEntity>)
+
+    @Transaction
+    @Query("SELECT * FROM characters")
+    fun getAllCharacters(): Flow<List<CharacterLocationRelation>>
 }
---
+++ app/src/main/java/com/acmelabs/rickandmortydex/data/database/relation/CharacterLocationRelation.kt
@@ -0,0 +1,16 @@
+package com.acmelabs.rickandmortydex.data.database.relation
+
+import androidx.room.Embedded
+import androidx.room.Relation
+import com.acmelabs.rickandmortydex.data.database.entity.CharacterEntity
+import com.acmelabs.rickandmortydex.data.database.entity.LocationEntity
+
+data class CharacterLocationRelation(
+    @Embedded
+    val character: CharacterEntity,
+    @Relation(
+        parentColumn = "originId",
+        entityColumn = "id"
+    )
+    val location: LocationEntity
+)
